fd4d317f6a3a10242562d04e21b6e8ee00bcb0ce
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-28 16:00:43 -0400

    Update voicetest.mjs

--- voicetest.mjs
+++ voicetest.mjs
@@ -11,8 +11,8 @@ const version = packageJson.version;
 
 const presence = { activities: [{ name: `voicetest v${version}`, type: 4 }], status: 'online' };
 
-const voice = 'sage';
-const filter = 'rubberband=pitch=1:tempo=1.0';
+const voice = 'ballad';
+const filter = 'rubberband=pitch=0.75:tempo=1.0';
 
 await createDiscord({
     log,
