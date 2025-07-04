9fd5d2ba85d8d283338d770fc01fb2048eff17bf
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-07-04 10:41:23 -0400

    Update sessionConfig.mjs

--- src/voiceOpenAI/openaiWebSocket/sessionConfig.mjs
+++ src/voiceOpenAI/openaiWebSocket/sessionConfig.mjs
@@ -9,8 +9,8 @@ export function getSessionConfig({ instructions, voice, tools }) {
         output_audio_format: 'pcm16',
         turn_detection: {
             type: 'server_vad',
+            threshold: 0.1,
             prefix_padding_ms: 300,
-            silence_duration_ms: 500,
             create_response: true,
             interrupt_response: true
         },
